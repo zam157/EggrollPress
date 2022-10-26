a9ce5a8bccc96cbb0e50a8e57cc0d2e1e3c74ed5
WT <24277775+zam157@users.noreply.github.com>
2022-10-27 01:03:18 +0800

    feat: image preview

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,12 +2,17 @@ import 'uno.css'
 import './styles/index.css'
 
 import DefaultTheme from 'vitepress/theme'
-import type { App } from 'vue'
+import type { EnhanceAppContext } from 'vitepress'
 import ImgWithTitle from './components/ImgWithTitle.vue'
+import { useMediumZoom, useMediumZoomProvider } from './composibles/useMediumZoom'
 
 export default {
   ...DefaultTheme,
-  enhanceApp({ app }: { app: App }) {
+  enhanceApp({ app, router }: EnhanceAppContext) {
     app.component('ImgWithTitle', ImgWithTitle)
+    useMediumZoomProvider(app, router) // enable image preview by default, see https://github.com/vuejs/vitepress/issues/854#issuecomment-1233866453
+  },
+  setup() {
+    useMediumZoom()
   },
 }
