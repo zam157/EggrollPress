4cca50f9975dfcd1b4ec338322d6c4bac4db6fca
WT <24277775+zam157@users.noreply.github.com>
2022-10-29 07:02:55 +0800

    chore: change the directory where the article files are stored; auto generate sidebar groups; i18n

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,50 +1,25 @@
 import { defineConfig } from 'vitepress'
 import Unocss from 'unocss/vite'
 
-// const i18n = {
-//   search: '搜索',
-//   menu: '菜单',
-//   toc: '本页目录',
-//   returnToTop: '返回顶部',
-//   appearance: '外观',
-//   previous: '前一篇',
-//   next: '下一篇',
-//   pageNotFound: '页面未找到',
-//   deadLink: {
-//     before: '你打开了一个不存在的链接：',
-//     after: '。'
-//   },
-//   deadLinkReport: {
-//     before: '不介意的话请提交到',
-//     link: '这里',
-//     after: '，我们会跟进修复。'
-//   },
-//   footerLicense: {
-//     before: '',
-//     after: ''
-//   },
-
-//   // aria labels
-//   ariaAnnouncer: {
-//     before: '',
-//     after: '已经加载完毕'
-//   },
-//   ariaDarkMode: '切换深色模式',
-//   ariaSkip: '直接跳到内容',
-//   ariaTOC: '当前页面的目录',
-//   ariaMainNav: '主导航',
-//   ariaMobileNav: '移动版导航',
-//   ariaSidebarNav: '侧边栏导航',
-// }
+import type { DefaultTheme } from 'vitepress/theme'
 
 const base = '/EggrollPress/'
 const faviconHref = `${base}favicon.png`
+
+type Chapter = '黑马奖' | '合订本' | '观众社论' | '线索' | '段子' | '带货' | '编读往来' | '体验' | '读书' | '另类参考' | '评论选登'
+const sidebarGroupGenerator = (title: string, chapters: Chapter[]): DefaultTheme.SidebarGroup => ({
+  collapsible: true,
+  collapsed: true,
+  text: title,
+  items: chapters.map(chapter => ({ text: chapter, link: `/club/${title}/${chapter}` })),
+})
 
 export default defineConfig({
   title: 'EggrollPress',
   description: '一个非官方蛋卷俱乐部刊物网站',
   lang: 'zh-CN',
   srcDir: 'src',
+  lastUpdated: true,
   base,
   head: [
     ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: faviconHref }],
@@ -59,125 +34,25 @@ export default defineConfig({
       { text: '✨征稿通知', link: '/solicit-contributions' },
     ],
     sidebar: [
-      {
-        collapsible: true,
-        collapsed: true,
-        text: '试刊(22.07.17)',
-        items: [
-          { text: '黑马奖', link: '/0/black-horse' },
-          { text: '合订本', link: '/0/bound-volume' },
-          { text: '观众社论', link: '/0/editorials' },
-          { text: '线索', link: '/0/clues' },
-          { text: '段子', link: '/0/memes' },
-        ],
-      },
-      {
-        collapsible: true,
-        collapsed: true,
-        text: '第一期(22.07.25)',
-        items: [
-          { text: '黑马奖', link: '/1/black-horse' },
-          { text: '线索', link: '/1/clues' },
-          { text: '观众社论', link: '/1/editorials' },
-          { text: '体验', link: '/1/experiences' },
-        ],
-      },
-      {
-        collapsible: true,
-        collapsed: true,
-        text: '第二期(22.08.01)',
-        items: [
-          { text: '黑马奖', link: '/2/black-horse' },
-          { text: '合订本', link: '/2/bound-volume' },
-          { text: '线索', link: '/2/clues' },
-          { text: '观众社论', link: '/2/editorials' },
-          { text: '段子', link: '/2/memes' },
-          { text: '体验', link: '/2/experiences' },
-          { text: '带货', link: '/2/commerce' },
-        ],
-      },
-      {
-        collapsible: true,
-        collapsed: true,
-        text: '第三期(22.08.09)',
-        items: [
-          { text: '黑马奖', link: '/3/black-horse' },
-          { text: '线索', link: '/3/clues' },
-          { text: '观众社论', link: '/3/editorials' },
-          { text: '编读往来', link: '/3/communication' },
-          { text: '体验', link: '/3/experiences' },
-          { text: '带货', link: '/3/commerce' },
-        ],
-      },
-      {
-        collapsible: true,
-        collapsed: true,
-        text: '第四期(22.08.16)',
-        items: [
-          { text: '黑马奖', link: '/4/black-horse' },
-          { text: '体验', link: '/4/experiences' },
-          { text: '线索', link: '/4/clues' },
-          { text: '观众社论', link: '/4/editorials' },
-          { text: '编读往来', link: '/4/communication' },
-          { text: '读书', link: '/4/read' },
-        ],
-      },
-      {
-        collapsible: true,
-        collapsed: true,
-        text: '第五期(22.08.24)',
-        items: [
-          { text: '黑马奖', link: '/5/black-horse' },
-          { text: '体验', link: '/5/experiences' },
-          { text: '线索', link: '/5/clues' },
-          { text: '观众社论', link: '/5/editorials' },
-          { text: '编读往来', link: '/5/communication' },
-          { text: '读书', link: '/5/read' },
-          { text: '另类参考', link: '/5/inverse' },
-        ],
-      },
-      {
-        collapsible: true,
-        collapsed: true,
-        text: '第六期(22.09.01)',
-        items: [
-          { text: '黑马奖', link: '/6/black-horse' },
-          { text: '编读往来', link: '/6/communication' },
-          { text: '观众社论', link: '/6/editorials' },
-          { text: '线索', link: '/6/clues' },
-          { text: '合订本', link: '/6/bound-volume' },
-          { text: '评论选登', link: '/6/comments' },
-        ],
-      },
-      {
-        collapsible: true,
-        collapsed: true,
-        text: '第七期(22.09.12)',
-        items: [
-          { text: '黑马奖', link: '/7/black-horse' },
-          { text: '体验', link: '/7/experiences' },
-          { text: '编读往来', link: '/7/communication' },
-          { text: '观众社论', link: '/7/editorials' },
-          { text: '线索', link: '/7/clues' },
-          { text: '读书', link: '/7/read' },
-          { text: '评论选登', link: '/7/comments' },
-        ],
-      },
-      {
-        collapsible: true,
-        collapsed: true,
-        text: '第八期(22.10.24)',
-        items: [
-          { text: '黑马奖', link: '/8/black-horse' },
-          { text: '读书', link: '/8/read' },
-          { text: '观众社论', link: '/8/editorials' },
-          { text: '体验', link: '/8/experiences' },
-          { text: '编读往来', link: '/8/communication' },
-          { text: '合订本', link: '/8/bound-volume' },
-          { text: '评论选登', link: '/8/comments' },
-        ],
-      },
+      sidebarGroupGenerator('第0期(22.07.17) 试刊', ['黑马奖', '合订本', '观众社论', '线索', '段子']),
+      sidebarGroupGenerator('第1期(22.07.25)', ['黑马奖', '线索', '观众社论', '体验']),
+      sidebarGroupGenerator('第2期(22.08.01)', ['黑马奖', '合订本', '线索', '观众社论', '段子', '体验', '带货']),
+      sidebarGroupGenerator('第3期(22.08.09)', ['黑马奖', '线索', '编读往来', '体验', '带货']),
+      sidebarGroupGenerator('第4期(22.08.16)', ['黑马奖', '体验', '线索', '观众社论', '编读往来', '读书']),
+      sidebarGroupGenerator('第5期(22.08.24)', ['黑马奖', '体验', '线索', '观众社论', '编读往来', '读书', '另类参考']),
+      sidebarGroupGenerator('第6期(22.09.01)', ['黑马奖', '编读往来', '观众社论', '线索', '合订本', '评论选登']),
+      sidebarGroupGenerator('第7期(22.09.12)', ['黑马奖', '体验', '编读往来', '观众社论', '线索', '读书', '评论选登']),
+      sidebarGroupGenerator('第8期(22.10.24)', ['黑马奖', '读书', '观众社论', '体验', '编读往来', '合订本', '评论选登']),
     ],
+    editLink: {
+      pattern: 'https://github.com/zam157/EggrollPress/edit/master/src/:path',
+      text: '在 GitHub 上编辑此页',
+    },
+    lastUpdatedText: '最近更新',
+    docFooter: {
+      prev: '上一篇',
+      next: '下一篇',
+    },
     // algolia: {
     //   indexName: 'eggroll_press',
     //   appId: 'MJAMQS9CDR',
@@ -191,5 +66,8 @@ export default defineConfig({
     plugins: [
       Unocss({}),
     ],
+  },
+  vue: {
+    reactivityTransform: true,
   },
 })
